bd888cdc877140d30b0930bc1183d4a30c72a541
Konrad Milewski <konradmilewski2000@interia.eu>
2024-07-07 12:47:01 +0200

    Added basic endpoints

--- src/CarRental.API/MinimalApi/ReservationEndpoints.cs
+++ src/CarRental.API/MinimalApi/ReservationEndpoints.cs
@@ -1,33 +1,43 @@
-// using MediatR;
-// using Microsoft.AspNetCore.Mvc;
-//
-// namespace CarRental.API.MinimalApi;
-//
-// public static class ReservationEndpoints
-// {
-//     private static async Task<IResult> GetTodoItemsWithPagination([AsParameters] GetTodoItemsWithPaginationQuery query,
-//         IMediator mediator)
-//     {
-//         var result = await mediator.Send(query);
-//
-//         return Results.Ok(result);
-//     }
-//
-//     private static async Task<IResult> CreateTodoItem([FromBody] CreateTodoItemCommand command,
-//         IMediator mediator)
-//     {
-//         var result = await mediator.Send(command);
-//
-//         return Results.Ok(result);
-//     }
-//
-//     public static void AddReservationEndpoints(this IEndpointRouteBuilder routeBuilder)
-//     {
-//         
-//         routeBuilder.MapPost("/todoitems", CreateTodoItem)
-//             .WithOpenApi();
-//
-//         routeBuilder.MapPut("/todoitems/{id}", GetTodoItemsWithPagination)
-//             .WithOpenApi();
-//     }
-// }
+using CarRental.Application.Reservations.Commands.CreateReservation;
+using MediatR;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarRental.API.MinimalApi;
+
+public static class ReservationEndpoints
+{
+    private static async Task<IResult> GetTodoItemsWithPagination([AsParameters] GetTodoItemsWithPaginationQuery query,
+        IMediator mediator)
+    {
+        var result = await mediator.Send(query);
+
+        return Results.Ok(result);
+    }
+
+    private static async Task<IResult> CreateReservation([FromBody] CreateReservationCommand command,
+        IMediator mediator)
+    {
+        var result = await mediator.Send(command);
+
+        return result.IsSuccess ? Results.Ok()
+        
+        return Results.Ok(result);
+    }
+    
+    private static async Task<IResult> GetReservations([AsParameters] GetReservationsQuery query,
+        IMediator mediator)
+    {
+        var result = await mediator.Send(command);
+
+        return Results.Ok(result);
+    }
+
+    public static void AddReservationEndpoints(this IEndpointRouteBuilder routeBuilder)
+    {
+        routeBuilder.MapGet("/reservations", GetReservations).WithOpenApi();
+        
+        routeBuilder.MapPost("/createReservation", CreateReservation)
+            .WithOpenApi();
+    }
+}
